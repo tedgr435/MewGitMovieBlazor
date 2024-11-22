c0266a6dbad67040618035207cae17902fd91d05
Josh Gronneberg <joshua.gronneberg@ndus.edu>
2024-11-22 14:13:02 -0600

    Movie DB and Links

---
+++ Data/MovieAppForGitContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieAppForGit;
+
+namespace MovieAppForGit.Data
+{
+    public class MovieAppForGitContext : DbContext
+    {
+        public MovieAppForGitContext (DbContextOptions<MovieAppForGitContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieAppForGit.Movie> Movie { get; set; } = default!;
+    }
+}
